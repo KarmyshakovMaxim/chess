0b303ac561bc5a3682c5897454449771e989bb85
Maxim Karmyshakov <mk@youkeeps.com>
2024-07-01 00:15:43 +0300

    feat(figures): add movement of figure

--- src/models/figures/Rook.ts
+++ src/models/figures/Rook.ts
@@ -5,11 +5,16 @@ import blackLogo from '../../assets/bR.png'
 import wihteLogo from '../../assets/wR.png'
 
 export class Rook extends Figure {
-  
-  
   constructor(color: Colors, cell: Cell) {
     super(color, cell)
     this.logo = color === Colors.BLACK ? blackLogo : wihteLogo;
     this.name = FigureNames.ROOK;
+  }
+
+  canMove(target: Cell): boolean {
+    if(!super.canMove(target)) return false;
+    if(this.cell.isEmptyVertical(target)) return true;
+    if(this.cell.isEmptyHorizontal(target)) return true;
+    return false;
   }
 }
